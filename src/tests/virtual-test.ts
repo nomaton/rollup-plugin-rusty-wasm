2982dc092938f6601fb25dbf12d527d270bdc8f2
nomaton <morione.nomaton@gmail.com>
2022-04-05 00:19:18 +0900

    Fix out of sync test code

--- src/tests/virtual-test.ts
+++ src/tests/virtual-test.ts
@@ -3,9 +3,9 @@ import test from "ava";
 import * as virtual from "../virtual";
 
 test("virtual.*: proxy id invariant", t => {
-    const wasmPathId = "a/b/c.wasm";
-    const id = virtual.composeVirtualId({ type: 'proxy', wasmPathId });
+    const wasmId = "a/b/c.wasm";
+    const id = virtual.composeVirtualId({ type: 'proxy', wasmId });
 
     t.true(virtual.isVirtualId(id));
-    t.deepEqual(virtual.decomposeVirtualId(id), { type: 'proxy', wasmPathId });
+    t.deepEqual(virtual.decomposeVirtualId(id), { type: 'proxy', wasmId });
 });
